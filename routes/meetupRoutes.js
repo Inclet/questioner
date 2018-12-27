f17b5c008bac1d80ec996bf68fc4de955d506b67
Clet Mwunguzi <mwunguzicl@gmail.com>
2018-12-27 19:32:30 +0200

    add feature for fetching a specific meetup record

--- routes/meetupRoutes.js
+++ routes/meetupRoutes.js
@@ -4,6 +4,7 @@ import meetupControllers from '../controllers/meetupControllers';
 const router = express.Router();
 
 router.post('/meetups', meetupControllers.create);
+router.get('/meetups/:id', meetupControllers.fetchMeetup);
 
 
 export default router;
