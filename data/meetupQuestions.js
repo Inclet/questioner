94d7944f240d06d7329f2df56a746ae1b397abef
Clet Mwunguzi <mwunguzicl@gmail.com>
2018-12-29 16:30:16 +0200

    add feature for upvoting a question

---
+++ data/meetupQuestions.js
@@ -0,0 +1,13 @@
+
+const meetupQuestions = [{
+	id : 1,
+	createdOn : "December 20, 2017",
+	createdBy : 3,
+	meetup : 2,
+	title : "Future Transport",
+	body : "we are moving into the new era...",
+	votes : 5
+}];
+
+
+export default meetupQuestions;
